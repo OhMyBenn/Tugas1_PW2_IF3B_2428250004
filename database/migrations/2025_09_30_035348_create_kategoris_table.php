ccbc84feba2ccd0321985b92965b5692a53f94b5
RubenAlfandoWhite <rubenalfandowhite2016@gmail.com>
2025-10-01 00:03:30 +0700

    New

--- database/migrations/2025_09_30_035348_create_kategoris_table.php
+++ database/migrations/2025_09_30_035348_create_kategoris_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
-            $table->double();
             $table->timestamps();
         });
     }
