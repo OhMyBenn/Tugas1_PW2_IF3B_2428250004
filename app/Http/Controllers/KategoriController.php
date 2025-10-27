2e7f37744b2d9af63d5242d61ecbcddcc1c6d517
RubenAlfandoWhite <rubenalfandowhite_2428250004@mhs.mdp.ac.id>
2025-10-27 20:05:45 +0700

    Update KategoriController.php

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -13,7 +13,7 @@ class KategoriController extends Controller
      */
     public function index()
     {
-        $kategori = Kategori::all();
+        $kategori = Kategori::all() -> get();
         return response()->json($kategori, 200);
     }
 
