5cd30b86764bc94f22121b230c85ba717b3f6817
RubenAlfandoWhite <rubenalfandowhite_2428250004@mhs.mdp.ac.id>
2025-10-31 23:03:39 +0700

    Update KategoriController.php

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -77,7 +77,7 @@ class KategoriController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Kategori $kategori, $id)
+    public function update(Request $request, $id)
     {
     // Validasi input dari request
         $validate = $request->validate([
